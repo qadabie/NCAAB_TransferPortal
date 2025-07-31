5a1c8fce659b6faa64f0dcf453f2adb02df36763
jacobrsansom <jrsansom@umich.edu>
2025-07-31 06:20:10 -0400

    removed any hardcoding

--- pd_fe/db_connect.py
+++ pd_fe/db_connect.py
@@ -1,20 +1,12 @@
 from dotenv import load_dotenv
 from sqlalchemy import create_engine
+import os
 
 # Load .env file
 load_dotenv()
 
-# as of 7/22/25 the database information is hardcoded so the instructional team can run the code
-# Advanced uses of this method would involve a personal .env file for each user using the following function
-
-# def get_connection():
-#     return create_engine(
-#         f"postgresql+psycopg2://{os.getenv('PG_USER')}:{os.getenv('PG_PASSWORD')}"
-#         f"@{os.getenv('PG_HOST')}:{os.getenv('PG_PORT')}/{os.getenv('PG_DATABASE')}"
-#     )
-
 def get_connection():
     return create_engine(
-        "postgresql+psycopg2://avnadmin:AVNS_qm_fxCQpBhTyFqQeDEq"
-        "@umich-siads699-ncaab-transferportal-umich-siads699-ncaab-transf.f.aivencloud.com:14357/defaultdb"
+        f"postgresql+psycopg2://{os.getenv('PG_USER')}:{os.getenv('PG_PASSWORD')}"
+        f"@{os.getenv('PG_HOST')}:{os.getenv('PG_PORT')}/{os.getenv('PG_DATABASE')}"
     )
